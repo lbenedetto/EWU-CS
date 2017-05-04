e533570e59c5c105d0a99412065a76b82be67c2e
larperdoodle <larsbenedetto@gmail.com>
2017-05-04 14:09:15 -0700

    Completed assignment

---
+++ CSCD-260 - Architecture & Organization/array.asm
@@ -0,0 +1,40 @@
+.data
+  prompt: .asciiz "Enter number to search for: "
+  numbers: .word 1,1,1,1,3,3,34,5,5,6,6,4,3
+.text
+main:
+  # print
+  li $v0, 4
+  la $a0, prompt
+  syscall
+
+  # read int
+  li $v0, 5
+  syscall
+  move $t0, $v0
+
+  # $t0 int to search for
+  # $t1 index
+  # $t2 number of occurances
+  # $t3 the array
+  # $t4 the absolute address
+  # $t5 the value at the ix
+
+    li $t1, 0
+    li $t2, 0
+    la $t3, numbers
+  loop:
+    bgt $t1,12,exit # for 0 through 12
+    add $t4, $t1, $t3 # find the address of that index
+    lw $t5, 0($t4) # put the value at ix in $t5
+    bne $t0, $t5, L1 # if != jump to incrementer
+    addi $t2, $t2, 1 # if == increment num occurances
+    L1: addi $t1,$t1,4 # increment ix by 1 (4 bytes)
+    j loop
+
+  exit:
+    # print the number of occurances
+    li $v0, 4
+    move $a0, $t2
+    li, $v0, 1
+    syscall
