12a5a252f4cecbf9eed5dbba4c7174886dab7707
larperdoodle <larsbenedetto@gmail.com>
2017-05-24 22:28:17 -0700

    updated assignment to store more things in the stack

--- CSCD-260 - Architecture & Organization/StackPointer.asm
+++ CSCD-260 - Architecture & Organization/StackPointer.asm
@@ -14,43 +14,51 @@
   li $v0, 5
   syscall
   move $t0, $v0
-  
   # $t0, int to search for
-  la $t1, array
-  # $t2 value at ix
-  add $t3, $t1, 36
-  li $t4, 0
-    
   jal search
   
   li $v0, 4
   la $a0, result
   syscall
   
-  move $a0, $t4
+  lw $t1, 0($sp)
+  move $a0, $t1
   li $v0, 1
   syscall
   
   j exit
   
   search:
+    # store the return link in the stack
     addi $sp, $sp, -4
-    sw $ra 0($sp)
+    sw $ra, 0($sp)
+
+    la $t1, array #load the address of the array into t1
+    addi $sp, $sp, -4
+    sw $t1, 0($sp) # load address of array into the stack
+    # $t2 value at ix
+    add $t3, $t1, 36 #store the end address of the array in $t3
+    li $t1, 0 # the value to keep track of the sum
   loop:
-    bgt $t1,$t3,return
-    lw $t2, 0($t1)
-    bgt $t0, $t2, increment
+    lw $t1, 0($sp)
+    bgt $t1, $t3, return
+    lw $t1, 0($t1)
+    bgt $t0, $t1, increment
       jal addToSum
     increment:
+    lw $t1, 0($sp)
     addi $t1, $t1, 4
+    sw $t1, 0($sp)
+
     j loop
     return:
+    addi $sp, $sp, 4
     lw $ra, 0($sp)
-    addi $sp, $sp, 4    
+    sw $t4, 0($sp)
     jr $ra
-    
-    addToSum:
-    add $t4, $t4, $t2
+
+  addToSum:
+    add $t4, $t4, $t1
     move $a0, $t4
     li $v0, 1
     syscall
@@ -58,6 +66,4 @@
     la $a0, newline
     syscall
     jr $ra
-    
-    
- exit:
+exit:
