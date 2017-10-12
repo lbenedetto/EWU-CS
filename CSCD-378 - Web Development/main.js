a2470afedd56638aa94790ad8507ef5c5d6d508b
larperdoodle <larsbenedetto@gmail.com>
2017-10-12 12:24:25 -0700

    Added code to update window hash

--- CSCD-378 - Web Development/main.js
+++ CSCD-378 - Web Development/main.js
@@ -1,8 +1,13 @@
+$(window).on("load", init);
+
 function navigateToHash() {
 	var hash = window.location.hash;
-	$(hash)[0].click().click();
+	$(hash)[0].click();
 }
 
-function showSelected(){
-
+function init() {
+	$(".nav").click(nav);
+}
+function nav(){
+	window.location.hash = "#" + $(this).get(0).id;
 }
