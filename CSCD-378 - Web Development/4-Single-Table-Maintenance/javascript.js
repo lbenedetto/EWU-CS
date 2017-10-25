e00d76df631a073d34358fffe57e23a018db9695
larperdoodle <larsbenedetto@gmail.com>
2017-10-24 23:29:57 -0700

    Switched to encodeURI

--- CSCD-378 - Web Development/4-Single-Table-Maintenance/javascript.js
+++ CSCD-378 - Web Development/4-Single-Table-Maintenance/javascript.js
@@ -1,8 +1,20 @@
 var $TABLE = $('#table');
 
-$('.table-edit').click()(function () {
-	$(this).parents('tr').removeClass()
+$('.table-edit').click(function () {
+	var button = $(this);
+	var children = button.closest('tr').find('td');
+	var isEdit = children.get(0).contentEditable;
+	if (isEdit === "true")
+		button.html("Edit");
+	else
+		button.html("Done");
+
+	for (var i = 1; i < children.length; i++) {
+		var child = children.get(i);
+		child.contentEditable = isEdit !== "true";
+	}
 });
+
 $('.table-remove').click(function () {
 	$(this).parents('tr').detach();
 });
