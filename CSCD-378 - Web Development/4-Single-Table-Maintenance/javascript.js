c2d076725719dda8e0d2710497f2f4d5336bb60d
larperdoodle <larsbenedetto@gmail.com>
2017-10-24 23:49:08 -0700

    Began adding PHP stuff

--- CSCD-378 - Web Development/4-Single-Table-Maintenance/javascript.js
+++ CSCD-378 - Web Development/4-Single-Table-Maintenance/javascript.js
@@ -1,14 +1,25 @@
 var $TABLE = $('#table');
+var HttpClient = function () {
+	this.get = function (aUrl, aCallback) {
+		$.ajax({
+			url: proxy + aUrl,
+			type: 'GET',
+			success: aCallback
+		});
+	}
+};
+var client = new HttpClient();
 
 $('.table-edit').click(function () {
 	var button = $(this);
 	var children = button.closest('tr').find('td');
 	var isEdit = children.get(1).contentEditable;
-	if (isEdit === "true")
+	if (isEdit === "true") {
 		button.html("Edit");
-	else
-		button.html("Done");
-
+	} else {
+		button.html("Save");
+		saveRow(children)
+	}
 	for (var i = 1; i < children.length; i++) {
 		var child = children.get(i);
 		child.contentEditable = isEdit !== "true";
@@ -23,3 +34,7 @@ $('.table-add').click(function () {
 	var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
 	$TABLE.find('table').append($clone);
 });
+
+function saveRow(fields){
+
+}
