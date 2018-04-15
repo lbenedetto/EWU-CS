d21c44841c063e66b1061803172906c6974c436c
larperdoodle <larsbenedetto@gmail.com>
2018-04-14 21:15:07 -0700

    Add interactivity

--- 499 - Self Directed Study/Flutter/Project 2/layouts_app/lib/main.dart
+++ 499 - Self Directed Study/Flutter/Project 2/layouts_app/lib/main.dart
@@ -88,15 +88,10 @@ class ImageTitle extends StatelessWidget {
               ],
             ),
           ),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          new Text('$numFavorites'),
+          new FavoriteCounter(),
         ],
       ),
     );
-
   }
 }
 
@@ -147,6 +142,53 @@ class ButtonBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: widgets,
       ),
+    );
+  }
+}
+
+class FavoriteCounter extends StatefulWidget {
+  @override
+  _FavoriteCounterState createState() => new _FavoriteCounterState();
+}
+
+class _FavoriteCounterState extends State<FavoriteCounter> {
+  bool _isFavorited = true;
+  int _fCount = 42;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _fCount--;
+        _isFavorited = false;
+      } else {
+        _fCount++;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(0.0),
+          child: new IconButton(
+            icon: (_isFavorited
+                ? new Icon(Icons.star)
+                : new Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        new SizedBox(
+          width: 18.0,
+          child: new Container(
+            child: new Text('$_fCount'),
+          ),
+        ),
+      ],
     );
   }
 }
