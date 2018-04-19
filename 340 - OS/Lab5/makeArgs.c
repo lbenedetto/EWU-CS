1fa455c81df46c634c183451ad226ced59f6404f
larperdoodle <larsbenedetto@gmail.com>
2018-04-18 20:17:48 -0700

    Finished Lab 3

--- 340 - OS/Lab5/makeArgs.c
+++ 340 - OS/Lab5/makeArgs.c
@@ -1,21 +1,25 @@
 #include "makeArgs.h"
 
-void clean(int argc, char **argv)
-{
+int countSpaces(char * s){
+	int size;
+	for (size=1; s[size]; s[size]==' ' ? size++ : *s++);
+	return size;
+}
 
+void clean(int argc, char **argv) {
+	free(argv);
 }// end clean
 
-void printargs(int argc, char **argv)
-{
+void printargs(int argc, char **argv) {
 	int x;
-	for(x = 0; x < argc; x++)
+	for (x = 0; x < argc; x++)
 		printf("%s\n", argv[x]);
 
 }// end printargs
 
-int makeargs(char *s, char *** argv)
-{
-	argv[0] = calloc(strlen(s), sizeof(char));
+int makeargs(char *s, char ***argv) {
+	int size = countSpaces(s);
+	argv[0] = malloc(size * sizeof(char **));
 	const char t = ' ';
 	char *token;
 	int i = 0;
@@ -23,12 +27,11 @@ int makeargs(char *s, char *** argv)
 	token = strtok(s, &t);
 
 	/* walk through other tokens */
-	while( token != NULL ) {
+	while (token != NULL) {
 		argv[0][i] = token;
 		token = strtok(NULL, &t);
 		i++;
 	}
-
 	return i;
 
 }// end makeArgs
