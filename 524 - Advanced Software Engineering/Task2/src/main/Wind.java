725b842ae2c5fbb251b743298045b9e6004885e0
larperdoodle <larsbenedetto@gmail.com>
2018-04-21 19:47:39 -0700

    Use better decodeChar solution thanks to some dude in the discord server

--- 524 - Advanced Software Engineering/Task2/src/main/Wind.java
+++ 524 - Advanced Software Engineering/Task2/src/main/Wind.java
@@ -6,33 +6,15 @@ public class Wind {
 		this.speed = speed;
 		this.direction = direction;
 	}
-	private static char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'};
 
-	//This is efficient but ugly
-	public static Wind decodeChar1(char c){
-		if(c == '.') return new Wind(0,0);
-		//z is an edge case because it is actually much higher of a value than the char after it
-		if(c == 'z') return new Wind(225, 10);
-		if ('a' <= c && c <= 'e') return new Wind(0, 10 + (c - 'a') * 10);
-		if ('f' <= c && c <= 'j') return new Wind(45, 10 + (c - 'f') * 10);
-		if ('k' <= c && c <= 'o') return new Wind(90, 10 + (c - 'k') * 10);
-		if ('p' <= c && c <= 't') return new Wind(135, 10 + (c - 'p') * 10);
-		if ('u' <= c && c <= 'y') return new Wind(180, 10 + (c - 'u') * 10);
-		//use @ instead of z because @ is what actually comes before A
-		if ('A' <= c && c <= 'D') return new Wind(225, 10 + (c - '@') * 10);
-		if ('E' <= c && c <= 'I') return new Wind(270, 10 + (c - 'E') * 10);
-		if ('J' <= c && c <= 'N') return new Wind(315, 10 + (c - 'J') * 10);
-		throw new RuntimeException("Invalid char passed to decodeChar");
-	}
+	public static Wind decodeChar(char c) {
+		if (('A' <= c && c <= 'N') || ('a' <= c && c <= 'z') || c == '.') {
+			int x = (c < 'a') ? c - 'A' + 26 : c - 'a';
+			if (c == '.') x = -1;
 
-	//This is pretty but inefficient
-	public static Wind decodeChar2(char c){
-		if(c == '.') return new Wind(0,0);
-		int i = 0;
-		for (int d = 0; d < 360; d += 45) {
-			for (int s = 10; s < 60; s += 10) {
-				if(chars[i++] == c) return new Wind(d, s);
-			}
+			int s = 10 + (x % 5) * 10;
+			int d = x / 5 * 45;
+			return new Wind(d, s);
 		}
 		throw new RuntimeException("Invalid char passed to decodeChar");
 	}
--- 524 - Advanced Software Engineering/Task2/src/test/WindTest.java
+++ 524 - Advanced Software Engineering/Task2/src/test/WindTest.java
@@ -7,26 +7,14 @@ public class WindTest {
 	private static char[] chars = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'};
 
 	@Test
-	void decodeValidChar1() {
-		compareWind(Wind.decodeChar1('.'), 0, 0);
+	void decodeValidChar() {
+		System.out.printf("testing %s, expecting %d,%d\n", '.', 0, 0);
+		compareWind(Wind.decodeChar('.'), 0, 0);
 		int i = 0;
 		for (int d = 0; d < 360; d += 45) {
 			for (int s = 10; s < 60; s += 10) {
 				System.out.printf("testing %s, expecting %d,%d\n", chars[i], d, s);
-				compareWind(Wind.decodeChar1(chars[i]), d, s);
-				i++;
-			}
-		}
-	}
-
-	@Test
-	void decodeValidChar2() {
-		compareWind(Wind.decodeChar2('.'), 0, 0);
-		int i = 0;
-		for (int d = 0; d < 360; d += 45) {
-			for (int s = 10; s < 60; s += 10) {
-				System.out.printf("testing %s, expecting %d,%d\n", chars[i], d, s);
-				compareWind(Wind.decodeChar2(chars[i]), d, s);
+				compareWind(Wind.decodeChar(chars[i]), d, s);
 				i++;
 			}
 		}
@@ -43,8 +31,7 @@ public class WindTest {
 			if(('A' <= c && c <= 'N') || ('a' <= c && c <= 'z') || c == '.') continue;
 			System.out.printf("Testing invalid char '%s'\n", c);
 			final char in = c;
-			assertThrows(RuntimeException.class, () -> Wind.decodeChar1(in));
-			assertThrows(RuntimeException.class, () -> Wind.decodeChar2(in));
+			assertThrows(RuntimeException.class, () -> Wind.decodeChar(in));
 		}
 	}
 }
