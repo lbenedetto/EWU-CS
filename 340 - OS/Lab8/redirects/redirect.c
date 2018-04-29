23ebe7e04406b7bb61c6589e4f252adea97354c6
larperdoodle <larsbenedetto@gmail.com>
2018-04-28 19:52:23 -0700

    File to command and generify

--- 340 - OS/Lab8/redirects/redirect.c
+++ 340 - OS/Lab8/redirects/redirect.c
@@ -12,32 +12,32 @@ bool checkRedirects(char *s, int *inCount, int *outCount) {
 	if (inCount > 0 || outCount > 0) return true;
 }
 
-void fileToCommandToFile(char *PATH, char *s) {
-
-}
-
-void fileToCommand(char *PATH, char *s) {
-
-}
-
-void commandToFile(char *PATH, char *s) {
+void genericRedirect(char *PATH, char *s, char *separator, char *fileType, FILE *replacementTarget) {
 	char **parts;
-	int argc = makeargs(s, &parts, ">");
+	int argc = makeargs(s, &parts, separator);
 	if (argc == 2) {
 		int status;
-		int filefd = open(parts[1], O_WRONLY | O_CREAT, 0666);
 		pid_t pid = fork();
 		if (pid == child) {
-			close(std_out);
-			dup2(filefd, std_out);
+			freopen(parts[1], fileType, replacementTarget);
 			forkIt(PATH, parts[0]);
-			close(filefd);
 			exit(0);
 		} else {
-			close(filefd);
 			waitpid(pid, &status, 0);
 		}
 	} else {
 		fprintf(stderr, "Incorrect args in commandToFile");
 	}
+}
+
+void fileToCommandToFile(char *PATH, char *s) {
+
+}
+
+void fileToCommand(char *PATH, char *s) {
+	genericRedirect(PATH, s, "<", "r", stdin);
+}
+
+void commandToFile(char *PATH, char *s) {
+	genericRedirect(PATH, s, ">", "w", stdout);
 }
