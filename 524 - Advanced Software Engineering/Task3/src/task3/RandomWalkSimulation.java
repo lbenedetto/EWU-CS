7878f2a58a52b1357bc89c00e1f6a21ab67b6e75
larperdoodle <larsbenedetto@gmail.com>
2018-05-07 16:25:34 -0700

    Slightly less fuck it

--- 524 - Advanced Software Engineering/Task3/src/task3/RandomWalkSimulation.java
+++ 524 - Advanced Software Engineering/Task3/src/task3/RandomWalkSimulation.java
@@ -11,8 +11,12 @@ public class RandomWalkSimulation {
 
 	public void simulate(int steps) {
 		for (int i = 0; i < iterations; i++) {
-			var walker = new RandomWalk(steps, 0, 0);
+			var walker = new RandomWalk(steps, 28, 4);
 			System.out.println(walker.execute(seed));
 		}
+	}
+
+	public static void main(String[] args) {
+		new RandomWalkSimulation(10, 1000).simulate(10000);
 	}
 }
--- 524 - Advanced Software Engineering/Task3/src/task3/Real.java
+++ 524 - Advanced Software Engineering/Task3/src/task3/Real.java
@@ -3,46 +3,72 @@ package task3;
 public class Real {
 	private int shift;
 	private int value;
+	private int valueBits;
+	private int shiftBits;
+	private int maxVal;
 
 	public Real(int valueBits, int shiftBits, float encodeValue) {
 		if (valueBits + shiftBits > 32) throw new RuntimeException("Invalid size (>32)");
 		var str = String.valueOf(encodeValue);
 		shift = str.indexOf('.');
+		if (shift == -1)
+			shift = str.length();
 		str = str.replace(".", "");
-		shift = str.length() - shift;
 		value = Integer.valueOf(str);
-	}
-
-	public static void main(String[] args) {
-		new Real(1, 1, 3.14159f);
+		this.valueBits = valueBits;
+		this.shiftBits = shiftBits;
+		this.maxVal = (int) Math.pow(2, valueBits) - 1;
+		if (value > maxVal)
+			throw new RuntimeException("Size exceeds max size capable of being stored in this representation in this configuration");
 	}
 
 	public Real add(Real b) {
-		return null;
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot add values of different value/shift bit sizes");
+		return new Real(valueBits, shiftBits, getValue() + b.getValue());
 	}
 
 	public Real subtract(Real b) {
-		return null;
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot subtract values of different value/shift bit sizes");
+		return new Real(valueBits, shiftBits, getValue() - b.getValue());
 	}
 
 	public Real multiply(Real b) {
-		return null;
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot multiply values of different value/shift bit sizes");
+		return new Real(valueBits, shiftBits, getValue() * b.getValue());
 	}
 
 	public Real divide(Real b) {
-		return null;
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot divide values of different value/shift bit sizes");
+		return new Real(valueBits, shiftBits, getValue() / b.getValue());
 	}
 
-	public Real interpolate(Real b) {
-		return null;
+	public Real interpolate(Real b, Real percent) {
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot interpolate values of different value/shift bit sizes");
+		return subtract(b).multiply(percent).add(this);
 	}
 
 	public int compare(Real b) {
-		return 0;
+		if (hasDifferentConfiguration(b))
+			throw new RuntimeException("Cannot compare values of different value/shift bit sizes");
+		return Float.compare(getValue(), b.getValue());
+	}
+
+	private boolean hasDifferentConfiguration(Real b) {
+		return valueBits != b.valueBits || shiftBits != b.shiftBits;
 	}
 
 	public float getValue() {
-		return 0f;
+		var str = String.valueOf(value);
+		str = String.format("%s.%s", str.substring(0, shift), str.substring(shift));
+		var fl = Float.valueOf(str);
+		if (fl > maxVal)
+			throw new RuntimeException("Size exceeds max size capable of being stored in this representation in this configuration");
+		return fl;
 	}
 
 }
