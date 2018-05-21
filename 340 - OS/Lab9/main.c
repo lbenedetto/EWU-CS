18fc418f31540da196c6684430891e300838ff30
larperdoodle <larsbenedetto@gmail.com>
2018-05-21 13:11:47 -0700

    Page number now correctly calculated

--- 340 - OS/Lab9/main.c
+++ 340 - OS/Lab9/main.c
@@ -16,30 +16,36 @@ char *readLine(FILE *fin) {
 	return NULL;
 }
 
-void readInt(FILE *fin, int *i) {
+void readInt(FILE *fin, uint32_t *i) {
 	char *temp = readLine(fin);
 	sscanf(temp, "%d", i);
 	free(temp);
 }
 
 struct PTE {
-	int pageFrame;
-	int currentBit;
+	uint32_t pageFrame;
+	uint32_t currentBit;
 };
 
 struct PF {
-	int pageNum;
+	uint32_t pageNum;
 };
+
+uint32_t countBits(uint32_t number) {
+	uint32_t count = 32;
+	for (uint32_t i = (uint32_t) 1 << (uint32_t) 31; i != 0; i >>= 1, count--)
+		if ((number & i) != 0) return count - 1;
+}
 
 int main() {
 	FILE *f1 = fopen("setup.txt", "r");
-	int vas, pageSize, pas;
+	uint32_t vas, pageSize, pas;
 	readInt(f1, &vas);
 	readInt(f1, &pageSize);
 	readInt(f1, &pas);
 	fclose(f1);
-	int numVirtualPages = vas / pageSize;
-	int numPhysicalPages = pas / pageSize;
+	uint32_t numVirtualPages = vas / pageSize;
+	uint32_t numPhysicalPages = pas / pageSize;
 
 	struct PTE pte[numVirtualPages];
 	struct PF pf[numPhysicalPages];
@@ -48,13 +54,17 @@ int main() {
 	char *line = readLine(f1);
 	while (line != NULL) {
 		uint32_t address;
-		uint32_t offset = address << (32 - pageSize);
-		offset = offset >> (32 - pageSize);
-
 		sscanf(line, "%d", &address);
 		free(line);
-		printf("Virtual Address: %d", address);
 
+		uint32_t numBitsPS = countBits(pageSize);
+		uint32_t offset = address << (32 - numBitsPS);
+		offset = offset >> (32 - numBitsPS);
+
+		printf("Virtual Address: %d\n", address);
+		printf("Page Number: %d\n", address >> numBitsPS);
+		printf("Page Frame Number: %d\n", 0);
+		printf("Physical Address: %d\n", 0);
 		line = readLine(f1);
 	}
 
